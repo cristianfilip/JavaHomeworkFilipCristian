03e4f1d28bbe7597fe3bd626412abecb413b473a
Cristian <Cristian@Cristian-PC>
2017-02-04 18:50:44 +0200

    My first project

---
+++ JavaHomeworkFilipCristian/src/myfirstpackage/MyFirstClass.java
@@ -0,0 +1,19 @@
+/*
+ * File of my first class writtem in java.
+ */
+package myfirstpackage;
+
+/**
+ * My first java class
+ * @author Cristian
+ */
+public class MyFirstClass {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("this is mother of opossum");
+    }
+    
+}
