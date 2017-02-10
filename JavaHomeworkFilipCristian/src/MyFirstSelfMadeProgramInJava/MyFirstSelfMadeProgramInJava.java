c6625a3d49a3f3591f14ed941b55a95383479fb7
Cristian <Cristian@Cristian-PC>
2017-02-11 00:45:27 +0200

    My first project

---
+++ JavaHomeworkFilipCristian/src/MyFirstSelfMadeProgramInJava/MyFirstSelfMadeProgramInJava.java
@@ -0,0 +1,8 @@
+package MyFirstSelfMadeProgramInJava;
+
+public class MyFirstSelfMadeProgramInJava {
+    
+    public static void main(String[] args) {
+        System.out.println("The program read this on my keyboard and showed it in the console.");
+    }
+}
--- JavaHomeworkFilipCristian/src/javahomeworkfilipcristian/JavaHomeworkFilipCristian.java
+++ JavaHomeworkFilipCristian/src/javahomeworkfilipcristian/JavaHomeworkFilipCristian.java
@@ -1,21 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package javahomeworkfilipcristian;
 
-/**
- *
- * @author Cristian
- */
 public class JavaHomeworkFilipCristian {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    
+    public static void main(String[] args) {  
     }
     
 }
