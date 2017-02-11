cff2abba1058f51335019dedb4868f294fff3319
Cristian <Cristian@Cristian-PC>
2017-02-11 16:23:13 +0200

    My first project

--- JavaHomeworkFilipCristian/src/MyFirstSelfMadeProgramInJava/MyFirstSelfMadeProgramInJava.java
+++ JavaHomeworkFilipCristian/src/MyFirstSelfMadeProgramInJava/MyFirstSelfMadeProgramInJava.java
@@ -1,8 +1,13 @@
 package MyFirstSelfMadeProgramInJava;
+import java.util.Scanner;
 
 public class MyFirstSelfMadeProgramInJava {
     
     public static void main(String[] args) {
-        System.out.println("The program read this on my keyboard and showed it in the console.");
+        Scanner scan = new Scanner (System.in);
+        System.out.println("What is your age ?");
+        System.out.print("Please type here: ");
+        String age = scan.nextLine();
+        System.out.println("So your sge is " + age);
     }
 }
