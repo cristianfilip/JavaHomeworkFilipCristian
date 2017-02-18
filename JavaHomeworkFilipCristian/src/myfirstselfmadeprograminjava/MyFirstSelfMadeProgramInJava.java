95ad3af3145c080a2ef1cc8e8428520730dd8098
Cristian <Cristian@Cristian-PC>
2017-02-18 16:57:45 +0200

    My first project

---
+++ JavaHomeworkFilipCristian/src/myfirstselfmadeprograminjava/MyFirstSelfMadeProgramInJava.java
@@ -0,0 +1,14 @@
+package myfirstselfmadeprograminjava;
+
+import java.util.Scanner;
+
+public class MyFirstSelfMadeProgramInJava {
+    
+    public static void main(String[] args) {
+        Scanner scan = new Scanner (System.in);
+        System.out.println("Cum te cheama ?");
+        System.out.print("Please type here: ");
+        String name = scan.nextLine();
+        System.out.println("Buna " + name + " ! Cati ani ai ?");
+    }
+}
